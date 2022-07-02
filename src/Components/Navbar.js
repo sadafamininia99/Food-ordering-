eed6c2137559c118606b8571b72641e01168eb65
SadafAmininiiiaa <s.amininia@tapsell.ir>
2022-07-02 22:38:37 +0430

    navbar styles

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,9 +10,9 @@ function Navbar() {
       </div>
       <div className="rightside">
         <Link to="/">Home</Link>
+        <Link to="/menu">menu</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
-        <Link to="/menu">menu</Link>
       </div>
     </div>
   );
