ad13af7d4c3fec7642a796db2d3cbba30d84d6f4
SadafAmininiiiaa <s.amininia@tapsell.ir>
2022-07-04 16:25:59 +0430

    navbar completed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,34 @@
-import React from "react";
-import logo from "../assets/logo.png";
+import React, { useState } from "react";
+import Logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
-import "./styles/Navbar.css"; 
+import ReorderIcon from "@material-ui/icons/Reorder";
+import "./styles/Navbar.css";
 
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
   return (
     <div className="navbar">
-      <div className="leftside">
-        <img src={logo} alt="logo" />
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
+        <img src={Logo} />
+        <div className="hiddenLinks">
+          <Link to="/"> Home </Link>
+          <Link to="/menu"> Menu </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/contact"> Contact </Link>
+        </div>
       </div>
-      <div className="rightside">
-        <Link to="/">Home</Link>
-        <Link to="/menu">menu</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+      <div className="rightSide">
+        <Link to="/"> Home </Link>
+        <Link to="/menu"> Menu </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/contact"> Contact </Link>
+        <button onClick={toggleNavbar}>
+          <ReorderIcon />
+        </button>
       </div>
     </div>
   );
