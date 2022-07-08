d015e2bebfb61a9705bfd06fc626ee5697b44ede
SadafAmininiiiaa <s.amininia@tapsell.ir>
2022-07-08 22:55:51 +0430

    header links and rounts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+        <switch>
+          <Route path="/" exact component={Home} />
+        </switch>
       </Router>
     </div>
   );
